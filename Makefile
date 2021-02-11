cf2553b88a5f5607a211b03077df685f391c05b3
Matthew Conger-Eldeen <mceldeen@gmail.com>
2021-02-10 17:50:26 -0700

    clean up rust code to be more functional

--- Makefile
+++ Makefile
@@ -1,7 +1,16 @@
 RUST_FILES := $(shell find . -type f -name '*.rs')
 
-target/debug/wip: $(RUST_FILES)
-	cargo build -j 8
+.PHONY: all
 
-target/release/wip: $(RUST_FILES)
-	cargo build -j 8 --release
+all: target/debug target/release
+
+target/debug/%: $(RUST_FILES)
+	cargo build -j 8 --bin $*
+
+target/release/%: $(RUST_FILES)
+	cargo build -j 8 --release --bin $*
+
+target/debug: target/debug/wip
+
+target/release: target/release/wip
+
--- src/main.rs
+++ src/main.rs
@@ -17,255 +17,274 @@ use std::fmt;
 use std::ops::Try;
 
 fn main() -> Result<(), WIPCLIError> {
-    let start = Instant::now();
-    let args: Vec<String> = env::args().skip(1).collect();
-    let wip_filename = get_wip_filename().map_err(|_| WIPCLIError::WIPFilenameNotFound)?;
-    let mut wip = WIP::new(wip_filename).map_err(|e| WIPCLIError::IOError(e))?;
-    if args.len() > 0 {
-        let command = args[0].as_str();
-        let command_args: Vec<&str> = args.iter().skip(1).map(|a| a.as_ref()).collect();
-        match command {
-            "push" => {
-                let item: Item = Item(command_args.join(" "));
-                if !item.is_empty() {
-                    wip.push(item).map_err(|e| WIPCLIError::IOError(e))
-                } else {
-                    Err(WIPCLIError::ArgumentError(String::from("nothing to push")))
-                }
-            }
-            "pop" => {
-                wip.pop().map_err(|e| WIPCLIError::IOError(e))
-            }
-            "focus" => {
-                let index: u64 = command_args.get(0)
-                    .into_result()
-                    .map_or_else(
-                        |_| Err(
-                            WIPCLIError::ArgumentError(String::from("no focus index supplied"))
-                        ),
-                        |i| i.parse::<u64>().map_err(|_|
-                            WIPCLIError::ArgumentError(String::from("index is not a number"))
-                        ),
-                    )?;
-                wip.focus(index).map_err(|e| WIPCLIError::IOError(e))
-            }
-            _ => {
-                Err(WIPCLIError::ArgumentError(format!("unrecognized command {}", command)))
-            }
-        }?
-    }
-    println!("{}", wip.show());
-    let show_timing = env::var("WIP_TIMING").map(|t| t == String::from("true")).unwrap_or(false);
-    if show_timing {
-        println!("done in {}µs", Instant::now().duration_since(start).as_micros());
-    }
-    Ok(())
+  let start = Instant::now();
+  let args: Vec<String> = env::args().skip(1).collect();
+  let wip_filename = get_wip_filename().map_err(|_| WIPCLIError::WIPFilenameNotFound)?;
+  let wip = WIP::new(wip_filename);
+  if args.len() > 0 {
+    let command = args[0].as_str();
+    let command_args: Vec<&str> = args.iter().skip(1).map(|a| a.as_ref()).collect();
+    match command {
+      "push" => {
+        let item: Item = Item(command_args.join(" "));
+        if !item.is_empty() {
+          wip.push(item).map_err(WIPCLIError::IOError)
+        } else {
+          Err(WIPCLIError::ArgumentError(String::from("nothing to push")))
+        }
+      }
+      "pop" => {
+        wip.pop().map_err(WIPCLIError::IOError)
+      }
+      "focus" => {
+        command_args.get(0)
+          .into_result()
+          .map_or_else(
+            |_| Err(
+              WIPCLIError::ArgumentError(String::from("no focus index supplied"))
+            ),
+            |i| i.parse::<u64>().map_err(|_|
+              WIPCLIError::ArgumentError(String::from("index is not a number"))
+            ),
+          )
+          .and_then(|index| {
+            wip.focus(index).map_err(WIPCLIError::IOError)
+          })
+      }
+      _ => {
+        Err(WIPCLIError::ArgumentError(format!("unrecognized command {}", command)))
+      }
+    }?
+  }
+  println!("{}", wip.show().map_err(WIPCLIError::IOError)?);
+  let wip_timing = env::var("WIP_TIMING")
+    .map_or_else(
+      |_| false,
+      |t| t == String::from("true"),
+    );
+  if wip_timing {
+    let runtime = Instant::now().duration_since(start);
+    println!("done in {}µs", runtime.as_micros());
+  }
+  Ok(())
 }
 
 enum WIPCLIError {
-    WIPFilenameNotFound,
-    IOError(io::Error),
-    ArgumentError(String),
+  WIPFilenameNotFound,
+  IOError(io::Error),
+  ArgumentError(String),
 }
 
 impl fmt::Debug for WIPCLIError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            WIPCLIError::WIPFilenameNotFound => {
-                write!(f, "if WIP_FILENAME was not specified so it defaulted to $HOME/.wip but HOME was not defined")
-            }
-            WIPCLIError::IOError(e) => {
-                write!(f, "io error: {}", e)
-            }
-            WIPCLIError::ArgumentError(e) => {
-                write!(f, "argument error: {}", e)
-            }
-        }
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      WIPCLIError::WIPFilenameNotFound => {
+        write!(f, "if WIP_FILENAME was not specified so it defaulted to $HOME/.wip but HOME was not defined")
+      }
+      WIPCLIError::IOError(e) => {
+        write!(f, "io error: {}", e)
+      }
+      WIPCLIError::ArgumentError(e) => {
+        write!(f, "argument error: {}", e)
+      }
     }
+  }
 }
 
 impl Termination for WIPCLIError {
-    fn report(self) -> i32 {
-        use WIPCLIError::*;
-        match self {
-            WIPFilenameNotFound => -1,
-            IOError(_) => -2,
-            ArgumentError(_) => -3,
-        }
-    }
+  fn report(self) -> i32 {
+    -1
+  }
 }
 
 fn get_wip_filename() -> Result<PathBuf, VarError> {
-    env::var("WIP_FILENAME")
-        .map(|wip_filename| {
-            Path::new(&wip_filename).to_path_buf()
-        })
-        .or_else(|_| {
-            env::var("HOME").map(|home| {
-                Path::new(&home).join(".wip")
-            })
-        })
+  env::var("WIP_FILENAME")
+    .map_or_else(
+      |_| env::var("HOME").map(
+        |home| Path::new(&home).join(".wip")
+      ),
+      |wip_filename| Ok(Path::new(&wip_filename).to_path_buf()),
+    )
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct WIP {
-    filename: PathBuf,
-    ops: Vec<Op>,
+  filename: PathBuf,
 }
 
 impl WIP {
-    pub fn new(filename: PathBuf) -> io::Result<WIP> {
-        let mut ops = vec![];
-        if let Ok(lines) = read_lines(filename.as_path()) {
-            for line in lines {
-                ops.push(serde_json::from_str(line?.as_str())?);
-            }
-        }
-        Ok(WIP { filename, ops })
-    }
+  pub fn new(filename: PathBuf) -> WIP {
+    WIP { filename }
+  }
 
-    pub fn items(&self) -> Vec<Item> {
-        let mut items = vec![];
-        for op in self.ops.iter() {
-            op.apply(&mut items);
-        }
+  pub fn ops(&self) -> io::Result<Vec<Op>> {
+    read_lines(self.filename.as_path()).and_then(
+      |lines| lines
+        .into_iter()
+        .map(
+          |line| line.and_then(|json|
+            serde_json::from_str(json.as_str())
+              .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+          )
+        )
+        .collect()
+    )
+  }
+
+  pub fn items(&self) -> io::Result<Vec<Item>> {
+    self.ops().map(
+      |ops| ops
+        .into_iter()
+        .fold(
+          vec![],
+          |items, op| op.apply(items),
+        )
+    )
+  }
+
+  pub fn show(&self) -> io::Result<String> {
+    self.items().map(|items| {
+      if items.len() == 0 {
+        String::from("No WIP")
+      } else {
         items
-    }
+          .into_iter()
+          .enumerate()
+          .rev()
+          .fold(
+            String::from(""),
+            |buf, (i, item)| format!("{}{}: {}\n", buf, i, item.to_string()),
+          )
+      }
+    })
+  }
 
-    pub fn show(&self) -> String {
-        let items = self.items();
-        if items.len() == 0 {
-            return String::from("No WIP");
-        }
-        let mut buf = String::from("");
-        for (i, item) in items.iter().enumerate().rev() {
-            buf += format!("{}: {}\n", i, item.to_string()).as_ref();
-        }
-        buf
-    }
+  pub fn push(&self, item: Item) -> io::Result<()> {
+    self.write_op(Op {
+      occurred_at: Local::now(),
+      payload: Payload::Push(item),
+    })
+  }
 
-    pub fn push(&mut self, item: Item) -> io::Result<()> {
-        self.write_op(Op {
-            occurred_at: Local::now(),
-            payload: Payload::Push(item),
-        })
-    }
+  pub fn pop(&self) -> io::Result<()> {
+    self.write_op(Op {
+      occurred_at: Local::now(),
+      payload: Payload::Pop,
+    })
+  }
 
-    pub fn pop(&mut self) -> io::Result<()> {
-        self.write_op(Op {
-            occurred_at: Local::now(),
-            payload: Payload::Pop,
-        })
-    }
+  pub fn focus(&self, index: u64) -> io::Result<()> {
+    self.write_op(Op {
+      occurred_at: Local::now(),
+      payload: Payload::Focus(index),
+    })
+  }
 
-    pub fn focus(&mut self, index: u64) -> io::Result<()> {
-        self.write_op(Op {
-            occurred_at: Local::now(),
-            payload: Payload::Focus(index),
-        })
-    }
-
-    fn write_op(&mut self, op: Op) -> io::Result<()> {
-        use std::fs::OpenOptions;
-        use std::io::Write;
-        let mut file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .append(true)
-            .open(self.filename.as_path())?;
-        writeln!(file, "{}", serde_json::to_string(&op).unwrap().as_str())?;
-        self.ops.push(op);
-        Ok(())
-    }
+  fn write_op(&self, op: Op) -> io::Result<()> {
+    use std::fs::OpenOptions;
+    use std::io::Write;
+    OpenOptions::new()
+      .write(true)
+      .create(true)
+      .append(true)
+      .open(self.filename.as_path())
+      .and_then(|mut file| {
+        serde_json::to_string(&op)
+          .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+          .and_then(|json| {
+            writeln!(file, "{}", json)
+          })
+      })
+  }
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+fn read_lines<P>(
+  filename: P
+) -> io::Result<io::Lines<io::BufReader<File>>>
+  where P: AsRef<Path>,
+{
+  File::open(filename)
+    .map(|file| io::BufReader::new(file).lines())
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Item(String);
 
 impl ToString for Item {
-    fn to_string(&self) -> String {
-        self.0.to_string()
-    }
+  fn to_string(&self) -> String {
+    self.0.to_string()
+  }
 }
 
 impl Item {
-    fn is_empty(&self) -> bool {
-        self.0.len() == 0
-    }
+  fn is_empty(&self) -> bool {
+    self.0.len() == 0
+  }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Op {
-    #[serde(with = "iso8601")]
-    pub occurred_at: DateTime<Local>,
+  #[serde(with = "iso8601")]
+  pub occurred_at: DateTime<Local>,
 
-    #[serde(flatten)]
-    pub payload: Payload,
+  #[serde(flatten)]
+  pub payload: Payload,
 }
 
 impl Op {
-    fn apply(&self, items: &mut Vec<Item>) -> Option<Item> {
-        self.payload.apply(items)
-    }
+  fn apply(self, items: Vec<Item>) -> Vec<Item> {
+    self.payload.apply(items)
+  }
 }
 
 mod iso8601 {
-    use chrono::{DateTime, TimeZone, Local};
-    use serde::{self, Deserialize, Serializer, Deserializer};
+  use chrono::{DateTime, TimeZone, Local};
+  use serde::{self, Deserialize, Serializer, Deserializer};
 
-    const FORMAT: &'static str = "%FT%T%z";
+  const FORMAT: &'static str = "%FT%T%z";
 
-    pub fn serialize<S>(
-        date: &DateTime<Local>,
-        serializer: S,
-    ) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
-    {
-        let s = format!("{}", date.format(FORMAT));
-        serializer.serialize_str(&s)
-    }
+  pub fn serialize<S>(
+    date: &DateTime<Local>,
+    serializer: S,
+  ) -> Result<S::Ok, S::Error>
+    where S: Serializer
+  {
+    let s = format!("{}", date.format(FORMAT));
+    serializer.serialize_str(&s)
+  }
 
-    pub fn deserialize<'de, D>(
-        deserializer: D,
-    ) -> Result<DateTime<Local>, D::Error>
-        where
-            D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        Local.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
-    }
+  pub fn deserialize<'de, D>(
+    deserializer: D,
+  ) -> Result<DateTime<Local>, D::Error>
+    where D: Deserializer<'de>
+  {
+    let s = String::deserialize(deserializer)?;
+    Local.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
+  }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type", content = "payload")]
 enum Payload {
-    Push(Item),
-    Pop,
-    Focus(u64),
+  Push(Item),
+  Pop,
+  Focus(u64),
 }
 
 impl Payload {
-    fn apply(&self, items: &mut Vec<Item>) -> Option<Item> {
-        match self {
-            &Payload::Push(ref item) => {
-                items.push(item.clone());
-                Some(item.clone())
-            }
-            &Payload::Pop => {
-                items.pop()
-            }
-            &Payload::Focus(index) => {
-                let removed = items.remove(index as usize);
-                items.push(removed.clone());
-                Some(removed)
-            }
-        }
+  fn apply(self, mut items: Vec<Item>) -> Vec<Item> {
+    match self {
+      Payload::Push(item) => {
+        items.push(item);
+      }
+      Payload::Pop => {
+        items.pop();
+      }
+      Payload::Focus(index) => {
+        let removed = items.remove(index as usize);
+        items.push(removed);
+      }
     }
+    items
+  }
 }
